94868b1192a48e745019c79daefc0689043b3970
Joel Kuiper <me@joelkuiper.eu>
2014-03-26 19:42:49 +0100

    Add build file for r.js

---
+++ static/build.js
@@ -0,0 +1,23 @@
+({
+    appDir: "./scripts",
+    baseUrl: "./",
+    dir: "./build",
+    optimize: "uglify2",
+
+    // call with `node r.js -o build.js`
+    // add `optimize=none` to skip script optimization (useful during debugging).
+
+    mainConfigFile: "./scripts/main.js",
+    preserveLicenseCsmments: false,
+
+    onBuildWrite: function (moduleName, path, singleContents) {
+        return singleContents.replace(/jsx!/g, '');
+    },
+
+    modules: [
+        {
+            name: "main",
+            exclude: ["jsx"]
+        }
+    ]
+})
