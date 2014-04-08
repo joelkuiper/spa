8b08c292ee1121b625660d56cba244c8a0924eb6
byron wallace <byron.wallace@gmail.com>
2014-04-08 17:07:37 -0400

    adding indexnumbers module

--- tokenizers.py
+++ tokenizers.py
@@ -5,6 +5,8 @@
 #
 
 from nltk.tokenize.punkt import *
+from functools import wraps
+from itertools import izip
 
 class CochraneNLPLanguageVars(PunktLanguageVars):
     _re_non_word_chars   = r"(?:[?!)\";}\]\*:@\'\({\[=\.])" # added =
@@ -33,6 +35,7 @@ class newPunktWordTokenizer(TokenizerI):
         """
         return [(sl.start, sl.stop) for sl in self._slices_from_text(text)]
 
+     
     def _slices_from_text(self, text):
         last_break = 0
         contains_no_words = True
@@ -46,5 +49,30 @@ class newPunktWordTokenizer(TokenizerI):
 word_tokenizer = newPunktWordTokenizer()
 sent_tokenizer = PunktSentenceTokenizer()
 
+def filters(func):
+    """
+    used as decorator
+    allows pipeline functions to return helpful
+    views/permetations of output data - flattened lists
+    and filters based on the base (hidden) features
+    """
+    @wraps(func)
+    def wrapper(self, *args, **kwargs):
+        
+        flatten = kwargs.pop("flatten", False)
+        filter = kwargs.pop("filter", None)
+
+        raw_output = func(self, *args, **kwargs)
+        if filter:
+            filtered_output = [[raw_word for raw_word, base_word in izip(raw_sent, base_sent) if filter(base_word)]
+                                for raw_sent, base_sent in izip(raw_output, self.functions)]
+        else:
+            filtered_output = raw_output
+
+        if flatten:
+            return [item for sublist in filtered_output for item in sublist]
+        else:
+            return filtered_output
+    return wrapper
 
 
