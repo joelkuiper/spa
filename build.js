e2fd57eba331e50be02d15aa4dc9fc618ecad80c
Joel Kuiper <me@joelkuiper.eu>
2014-04-10 18:32:53 +0200

    Also minify CSS
    - Update require-jsx (allows for source maps)

--- build.js
+++ build.js
@@ -1,16 +1,19 @@
 ({
-    appDir: "./scripts",
-    baseUrl: "./",
+    appDir: "./static",
+    baseUrl: "./scripts",
     dir: "./build",
     optimize: "uglify2",
+    useStrict: true,
 
     // call with `node r.js -o build.js`
     // add `optimize=none` to skip script optimization (useful during debugging).
 
-    mainConfigFile: "./scripts/main.js",
+    mainConfigFile: "./static/scripts/main.js",
     onBuildWrite: function (moduleName, path, singleContents) {
         return singleContents.replace(/jsx!/g, '');
     },
+
+    optimizeCss: 'standard',
 
     modules: [
         {
